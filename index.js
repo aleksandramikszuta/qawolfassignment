660665ace2d9e068495c30b5e92e4bb7217cadc5
Aleksandra Mikszuta <ola.mikszuta@gmail.com>
2024-07-26 16:48:25 +0200

    Beginning the main loop

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ async function sortHackerNewsArticles() {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
+  const newest = page.locator("a[href='newest']");
+  const age = page.locator('.age');
+  const more = page.locator('.morelink');
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+  await newest.click();
+  let ages = await age.all();
+  let currentAgeIndex = 0;
+  let currentAge = ages[currentAgeIndex].getAttribute("title");
+
+
+  for(let i=1; i<100; i++) {
+    if (currentAgeIndex > ages.length) {
+      await next.click();
+    }
+  }
 }
 
 (async () => {
